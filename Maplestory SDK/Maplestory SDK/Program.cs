8648315f1f1252365d8b1bfcdcff98a35165c41b
SND\namkazt_cp <SND\namkazt_cp@555e8bc2-ad3d-4a22-a156-a42dcab9bd6f>
2010-12-14 23:35:25 +0000

    base
    git-svn-id: https://mssdk.svn.codeplex.com/svn@4945 555e8bc2-ad3d-4a22-a156-a42dcab9bd6f

---
+++ Maplestory SDK/Maplestory SDK/Program.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace Maplestory_SDK
+{
+#if WINDOWS || XBOX
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            using (Run game = new Run())
+            {
+                game.Run();
+            }
+        }
+    }
+#endif
+}
+
